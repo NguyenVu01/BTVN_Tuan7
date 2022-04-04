02ee8dfea4daab270cc971b2c4e059fc44144445
NguyenVu01 <83327263+NguyenVu01@users.noreply.github.com>
2022-04-04 15:27:42 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace DemoDictionary
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
             TuDien tuDien = new TuDien();
             bool Run = true;
 
